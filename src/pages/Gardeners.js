61bfc1ce99d1e7c1895b45f2bbf00b522857f11d
lauragreen <lollymay@gmail.com>
2019-08-05 16:55:19 +0100

    Mock gardeners

--- src/pages/Gardeners.js
+++ src/pages/Gardeners.js
@@ -2,7 +2,23 @@ import React from "react";
 
 class GardenersPage extends React.Component {
   render() {
-    return <p>some gardeners</p>;
+    return (
+      <div>
+        <h1>Gardeners</h1>
+
+        <label for="gardener-select">Choose a gardener:</label>
+
+        <select id="gardener-select">
+          <option value="">--Please choose--</option>
+          <option value="dog">Dog</option>
+          <option value="cat">Cat</option>
+          <option value="hamster">Hamster</option>
+          <option value="parrot">Parrot</option>
+          <option value="spider">Spider</option>
+          <option value="goldfish">Goldfish</option>
+        </select>
+      </div>
+    );
   }
 }
 
