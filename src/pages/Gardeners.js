f9612b6f708a911e8b4242517104f579bc62698c
lauragreen <lollymay@gmail.com>
2019-08-06 16:42:20 +0100

    Construct list in gardeners page

--- src/pages/Gardeners.js
+++ src/pages/Gardeners.js
@@ -1,10 +1,34 @@
 import React from "react";
 
 class GardenersPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { list: null };
+  }
+  async componentDidMount() {
+    const response = await fetch(
+      "https://now-mongo-answers.lollymay.now.sh/list",
+      {
+        // const response = await fetch("http://localhost:3000/list", {
+        // mode: "no-cors"
+      }
+    );
+    const list = await response.json();
+    this.setState({ list });
+    console.log(response);
+  }
   render() {
     return (
       <div>
         <h1>Gardeners</h1>
+
+        <select id="select">
+          <option value="">--Please choose--</option>
+          {this.state.list &&
+            this.state.list.gardeners.map(({ _id, name }) => (
+              <option value={_id}>{name}</option>
+            ))}
+        </select>
 
         <label htmlFor="gardener-select">Choose a gardener:</label>
 
