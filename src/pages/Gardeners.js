02eb2ba54956b5bd349638c3fb14cda12e51050b
lauragreen <lollymay@gmail.com>
2019-08-06 18:16:06 +0100

    Change display from select to table
    - also CORS setHeader

--- src/pages/Gardeners.js
+++ src/pages/Gardeners.js
@@ -7,40 +7,39 @@ class GardenersPage extends React.Component {
   }
   async componentDidMount() {
     const response = await fetch(
-      "https://now-mongo-answers.lollymay.now.sh/list",
-      {
-        // const response = await fetch("http://localhost:3000/list", {
-        // mode: "no-cors"
-      }
+      "https://now-mongo-answers.lollymay.now.sh/list"
     );
     const list = await response.json();
     this.setState({ list });
-    console.log(response);
+    // console.log(response);
   }
   render() {
     return (
       <div>
         <h1>Gardeners</h1>
 
+        {/* <label htmlFor="gardener-select">Choose a gardener:</label>
         <select id="select">
           <option value="">--Please choose--</option>
           {this.state.list &&
             this.state.list.gardeners.map(({ _id, name }) => (
               <option value={_id}>{name}</option>
             ))}
-        </select>
+        </select> */}
 
-        <label htmlFor="gardener-select">Choose a gardener:</label>
-
-        <select id="gardener-select">
-          <option value="">--Please choose--</option>
-          <option value="dog">Dog</option>
-          <option value="cat">Cat</option>
-          <option value="hamster">Hamster</option>
-          <option value="parrot">Parrot</option>
-          <option value="spider">Spider</option>
-          <option value="goldfish">Goldfish</option>
-        </select>
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>_id</th>
+          </tr>
+          {this.state.list &&
+            this.state.list.gardeners.map(({ _id, name }) => (
+              <tr>
+                <td>{name}</td>
+                <td>{_id}</td>
+              </tr>
+            ))}
+        </table>
       </div>
     );
   }
