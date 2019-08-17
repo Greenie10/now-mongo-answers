0a057fe7076511945524b7f3e47db2e29d90a841
lauragreen <lollymay@gmail.com>
2019-08-17 14:20:56 +0100

    Add google fonts Libre Baskerville for text & Cabin for headings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import styled from "styled-components";
+
 class App extends React.Component {
   render() {
-    return (
-      <div>
-        <h1>Home</h1>
-      </div>
-    );
+    return <MainHeader>Home</MainHeader>;
   }
 }
+const MainHeader = styled.h1`
+  font-family: "Arial Black", "Arial Bold", Gadget, sans-serif;
+  font-weight: bold;
+  font-size: 36px;
+`;
 export default App;
