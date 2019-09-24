b55d79fd2e79fd58ae2add8c69accaca1e57bede
lauragreen <lollymay@gmail.com>
2019-09-24 10:06:35 +0100

    Add ListUniqueLocations

--- src/Components/list-questions.js
+++ src/Components/list-questions.js
@@ -26,12 +26,22 @@ export function ListAllQuestions() {
     <div key={id}>
     <p>{Location} {Zone} - <b>{Question}</b> {Date}<br />
     
-    {Answers.map(({ Gardener, AnAnswer }) => (
-      <span>{Gardener}: {AnAnswer}</span>
+    {Answers.map(({Gardener, AnAnswer }) => (
+      <span key={Gardener}><b>{Gardener}</b>: {AnAnswer}</span>
     ))},
     </p>
     </div>
     )
   )
 };
+
+export function ListUniqueLocations() {
+  const { loading, error, data } = useQuery(GET_QUESTIONS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  const unique =  [...new Set(data.getQuestions.map(elem => elem.Location))];
+  return unique.map((loc, index) => (
+    <p key={index}>{loc}</p>
+  ))
+}
 
--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,13 +1,12 @@
 import React from "react";
-import {ListAllQuestions} from '../Components/list-questions';
+import {ListUniqueLocations} from '../Components/list-questions';
 
 
 const QuestionsPage = () => {
     return (
       <div>
         <h1>Questions</h1>
-        <h2>List locations</h2>
-        <ListAllQuestions />
+        <ListUniqueLocations />
         
       </div>
     );
