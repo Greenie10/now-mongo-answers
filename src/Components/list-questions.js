71d0f4d0713e0c354d31a6141f69b32ccba30732
lauragreen <lollymay@gmail.com>
2019-10-18 07:18:26 +0100

    Clean up experimental queries

--- src/Components/list-questions.js
+++ src/Components/list-questions.js
@@ -1,9 +1,15 @@
 import React from "react";
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 
-import {QuestionWrapper, QuestionH4, AnswersList, Fleuron, ZoneBox} from './styled-components';
-import { zoneConstants } from '../constants';
+import {
+  QuestionWrapper,
+  QuestionH4,
+  AnswersList,
+  Fleuron,
+  ZoneBox
+} from "./styled-components";
+import { zoneConstants } from "../constants";
 
 const GET_QUESTIONS = gql`
   query GetQuestions {
@@ -13,7 +19,7 @@ const GET_QUESTIONS = gql`
       Location
       Zone
       Date
-      Answers { 
+      Answers {
         Gardener
         AnAnswer
       }
@@ -21,66 +27,36 @@ const GET_QUESTIONS = gql`
   }
 `;
 
-const GET_ZONES = gql`
-  query GetZones {
-    getZones(zone: zone) {
-      Question
-      Location
-    }
-  }
-`;
-
-// query ZonesQuery($zone: String) {
-//   getZones(zone: $zone) {
-//     Question
-//     Location
-//   }
-// }
-
 export function GetAllQuestions() {
   const { loading, error, data } = useQuery(GET_QUESTIONS);
   const { ZONE } = zoneConstants;
-  console.log("*****: GetAllQuestions -> data", data);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
-  return data.getQuestions.map(({ id, Question, Location, Zone, Date, Answers }) => (
-    <QuestionWrapper key={id}>
-      <QuestionH4><b>{Question}</b> - <em>{Location}</em> <ZoneBox zone={ZONE[Zone]}>{Zone}</ZoneBox></QuestionH4>
-      <AnswersList>
-        {Answers.map(({Gardener, AnAnswer }) => (
-          <>
-          <dt key={Gardener}>{Gardener}</dt>
-          <dd>{AnAnswer}</dd>
-          </>
-        ))}
-      </AnswersList>
-    <Fleuron>&#10086;</Fleuron>
-    </QuestionWrapper>
+  return data.getQuestions.map(
+    ({ id, Question, Location, Zone, Date, Answers }) => (
+      <QuestionWrapper key={id}>
+        <QuestionH4>
+          <b>{Question}</b> - <em>{Location}</em>{" "}
+          <ZoneBox zone={ZONE[Zone]}>{Zone}</ZoneBox>
+        </QuestionH4>
+        <AnswersList>
+          {Answers.map(({ Gardener, AnAnswer }) => (
+            <>
+              <dt key={Gardener}>{Gardener}</dt>
+              <dd>{AnAnswer}</dd>
+            </>
+          ))}
+        </AnswersList>
+        <Fleuron>&#10086;</Fleuron>
+      </QuestionWrapper>
     )
-  )
-};
-export function GetZones({ zone }) {
-  console.log("*****: GetZones -> zone", zone);
-  const { loading, error, data } = useQuery(GET_ZONES);
-  console.log("*****: GetZones -> data", data);
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-  return data.getZones.map(({ id, Question, Location }) => (
-    <div key={id}>
-    <p><b>{Question}</b> - <em>{Location}</em>{zone}</p>
-    </div>
-    )
-  )
-};
+  );
+}
 
 export function ListUniqueLocations() {
   const { loading, error, data } = useQuery(GET_QUESTIONS);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
-  const unique =  [...new Set(data.getQuestions.map(elem => elem.Location))];
-  return unique.map((loc, index) => (
-    <p key={index}>{loc}</p>
-  ))
+  const unique = [...new Set(data.getQuestions.map(elem => elem.Location))];
+  return unique.map((loc, index) => <p key={index}>{loc}</p>);
 }
-
-
--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,18 +1,13 @@
 import React from "react";
-// import {ListUniqueLocations} from '../Components/list-questions';
-import {GetAllQuestions} from '../Components/list-questions';
-// import {GetZones} from '../Components/list-questions';
-
+import { GetAllQuestions } from "../Components/list-questions";
 
 const QuestionsPage = () => {
-    return (
-      <main>
-        <h1>Questions</h1>
-        {/* <GetAllQuestions date="20161016"/> */}
-        <GetAllQuestions zone='8b'/>
-        
-      </main>
-    );
-  }
+  return (
+    <main>
+      <h1>Questions</h1>
+      <GetAllQuestions />
+    </main>
+  );
+};
 
 export default QuestionsPage;
